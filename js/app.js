90f817dbb213d1431dffb2185c22db155670fe23
Rhaegallb <mcorentinb@gmail.com>
2018-03-02 16:57:11 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,35 @@
+/** A pas peur tu peux le faire ai confiance en toi **/
+/**function add(a, b){
+  resultat = b + a;
+}
+function subs(a, b){
+  resultat = b - a;
+}
+function mutl(a, b){
+  resultat = (b * a);
+}
+function divise(a, b){
+  if(nbr2 == 0){                                   /** pour éviter les virgules du modulo **/
+    /**resultat = "error"
+  }else{
+    resultat = b / a;
+  }
+}
+var nom de la variable = document.getElementById("ce qu'on veut écouter");       /** pointeur **/
+/**nom de la variable.addEventListener("nom de l'évènement que l'on veut écouter");  /** écouteur **/
+/**var nom de la variable = ("#nom de l'élément dont on veux récup l'écrit").val();  /** récup de la valeur du champ **/
+
+
+
+  /**var elem = document.getElementById("ipt1");
+  console.log (elem);**/
+
+
+
+
+/**function addFixes = (input1,input2,input3,input4,input5,input6,input7,input8,input9,input10,input11)
+  result = input1 + input2 + input3 + input4 + input5 + input6 + input7 + input8 + input9 + input10 + input11;**/
+
+
+
+
